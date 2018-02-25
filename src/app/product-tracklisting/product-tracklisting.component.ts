f6761129ba71dbbb5b79d22385b44909fe2d3395
gupta89aman <gupta89aman@gmail.com>
2018-02-25 18:46:33 +0530

    Add files via upload

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -7,12 +7,12 @@ import { Album } from '../album';
   styleUrls: ['./product-tracklisting.component.css']
 })
 export class ProductTracklistingComponent implements OnInit {
-  _albumInfo : Album;
+  albumInfo : Album;
   constructor(private _productService : ProductService) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => 
-      this._albumInfo = response);
+      this.albumInfo = response);
   }
 
 }
